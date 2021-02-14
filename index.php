6ddbcac5ef53b96c187c48b6262dde66fe1ee95f
Marek <mmareq@gmail.com>
2021-02-14 11:54:52 +0800

    0.2s

---
+++ index.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Marek Sivák</title>
+    <meta name="description" content="Marek Sivák is a Product Designer based in Hong Kong.">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="theme-color" content="#ffffff">
+	<meta property="og:image" content="img/ms-cover.png" />
+	<meta property="og:title" content="Marek Sivák" />
+    <meta property="og:description" content="Marek Sivák is a Product Designer based in Hong Kong" />
+    <meta property="og:author" content="Marek Sivák" />
+	
+
+
+      <META NAME="ROBOTS" CONTENT="INDEX, FOLLOW" />
+     
+
+    <link href="img/ms-icon.jpg" sizes="192x192" rel="icon" />
+    <link rel="apple-touch-icon" href="img/ms-icon.png">
+ 
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/flexboxgrid3.min.css" type="text/css" />
+
+    <link rel="stylesheet" href="css/mareksiv.css" type="text/css" />
+    <link rel="stylesheet" href="css/mareksiv-additional.css" type="text/css" />
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" />
+
+</head>
+<body class="container">
+
+    <div class="container header">
+        <div class="row"><a class="no-style" href="#">Marek Sivák</a> <span>Product Designer</span></div>
+    </div>
+
+    <div class="container my-links">
+        <div class="row"><a target=_blank href="https://instagram.com/mareksiv"><span>Instagram</span></a> <span class="divider">/</span> 
+            <a target=_blank href="https://twitter.com/letoslaf"><span>Twitter</span></a> <span class="divider">/</span> 
+            <a target=_blank href="https://www.linkedin.com/in/marek-sivak/"><span>Linkedin</span></a> <span class="divider">/</span> 
+            <a target=_blank href="https://dribbble.com/leto"><span>Dribbble</span></a> </span> 
+        </div>
+        <div class="row"><a href="mailto: xsivak@gmail.com"><span>Mail</span></a></div>
+    </div>
+    
+    <div class="container intro">
+        <div class="row">
+            <p>
+                Hello—I make friendly software products. I am curious about people <!--I am serving -->and I use technology to delight them.
+            </p>
+            <p>
+                I work at Lalamove, which is a Hong Kong-based unicorn company expanding its logistics platform globally. I also help amazing teams to start their businesses. With Oqqur we launched their first virtual 360 tours. With Foundlost we organised challenging expeditions. Now I am helping athletes to tell stories about their workouts.
+            </p>
+        </div>
+    </div>
+<!--
+    <div class="container intro">
+        <div class="row">
+            <p>
+                Hello—I build friendly software products.
+            </p>
+            <p>
+                Born in Slovakia, moved to Hong Kong. I always felt delighted when I created something. To give a life to an idea. To make something that didn’t exist before.
+            </p>
+            <p>
+                My interest in tech brought me where I am now. I think how to make something better. I work at Lalamove, which is a unicorn company expanding it’s logistics platform globally. I also help amazing teams to launch their business.
+            </p>
+        </div>
+    </div>
+-->
+    <div class="container projects">
+        <div class="row">
+            <p>
+                Where I got involved recently <span>2020-2021</span>
+            </p>
+        </div>
+        <div class="row proj">
+            <div class="name">
+                Foundlost
+            </div>
+            <div class="desc">
+                Foundlost offers challenging expeditions to remote places of the planet. We figured out how to talk about expeditions, made booking system, design system for scaling the website and more
+            </div>
+            <div class="more">
+                <a href="./foundlost/">Read more</a>
+            </div>
+            <div class="image">
+                <a href="./foundlost/"><img style="max-width:800px" src="img/foundlost.jpg"></a>
+            </div>
+        </div> 
+        <div class="row proj">
+            <div class="name">
+                Oqqur
+            </div>
+            <div class="desc">
+                Oqqur builds live streaming platform for virtual tours in 360 reality. What team does gives a previe of post-covid world where travelling can happen from your home. Team needed critical product enhancements. I help them to make them and launch their first tours
+            </div>
+            <div class="more">
+                <a href="./oqqur/">Read more</a>
+            </div>
+            <div class="image">
+                <a href="./oqqur/"><img style="max-width:800px" src="img/oqqur.jpg"></a>
+            </div>
+        </div> 
+        
+        <div class="row proj">
+            <div class="name">
+                Lalamove
+            </div>
+            <div class="desc">
+                Lalamove makes change by empowering people to easily transfer their goods across the cities. I worked on series of projects that enhanced experience of users when they make payments on logistics platform
+            </div>
+            <div class="more">
+                <a style="display:none" target=_blank href="#">Read more</a>
+            </div>
+        </div> 
+        <div class="row proj">
+            <div class="name">
+                Levice: Kedysi a Dnes
+            </div>
+            <div class="desc">
+                A personal project that compares old photos of my home town with Google StreetView. I made design, wrote code and spent hundreds of hours in Photoshop to achieve pixel-perfect matching photos
+
+            </div>
+            <div class="more">
+                <a target=_blank href="https://levicekedysiadnes.sk">Visit website</a>
+            </div>
+            <div class="image">
+                <a target=_blank href="https://levicekedysiadnes.sk"><img style="max-width:800px" src="img/levice/levice-kedysi-a-dnes.jpg"></a>
+            </div>
+        </div> 
+    </div>
+
+    <div class="container they-said">
+        <div class="row">
+            <div class="col-md-6 awesome">
+                <div class="name">Jesus Gaytan</div>
+                <div class="bio">Product Leader (Zappos, Amazon)</div>
+                <div class="quote">Marek is very intelligent and capable. He has a strong design aesthetic that stems from solving people-problems. He has a calm leadership style and is an excellent listener. His work ethic is very strong, with a solid disposition to roll-up sleeves and get stuff done. I have no reservations recommending him and would gladly have him on my team any time.
+                </div>
+            </div>
+
+            <div class="col-md-6 awesome">
+                <div class="name">Christopher Schrader</div>
+                <div class="bio">Founder (24 Hour Race)</div>
+                <div class="quote">I have worked with dozens of designers at many companies and Marek stands out as an exceptional designer and leader. Marek is an outstanding leader, a thoughtful listener and exceptionally hard worker. Marek is obsessed with learning, he wants to live and breath the lives of his customers and stakeholders and constantly learn the cutting-edge best design practices. His background and experience mean that he is acutely aware of the importance of design in improving customer experience and driving revenues. I would not hesitate to work with Marek (and indeed still do!) and highly recommend him to those who have the opportunity to work with him.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container thats-me">
+        <div class="row">
+            <img style="max-width:500px" src="img/mareksiv2021.jpg">
+            
+        </div>
+
+ 
+    </div>
+
+    
+
+    
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-154783588-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154783588-1');
+</script> 
+    
+</body>
+</html>
